5ce575203d4ad46986758f418aecaea2d645b755
richard <liuyngchng@hotmail.com>
2022-08-26 18:01:50 +0800

    debug

--- sin.py
+++ sin.py
@@ -7,6 +7,7 @@
 一元三次方程能够符合数据样本的特征
 需要求解的一元三次方程 y = a + b x + c x^2 + d x^3，
 事实上需要求解的是a, b, c, d 4个系数
+需要事先pip install numpy，安装numpy这个python库
 '''
 import numpy as np
 import math
@@ -25,6 +26,10 @@ d = np.random.randn()
 
 # 每次调整a、b、c、d数据的步伐大小
 learning_rate = 1e-6
+#循环2000次，函数收敛，当然也可以不指定次数，可以将条件设定为
+# 拟合函数和样本值（x，y）的误差小于某个值
+# 这里指定2000次，是因为上文的样本一共就2000个，一般来说迭代次数
+#要小于样本数量
 for t in range(2000):
     # Forward pass: compute predicted y
     # y = a + b x + c x^2 + d x^3
@@ -43,6 +48,7 @@ for t in range(2000):
     # Backprop to compute gradients of a, b, c, d with respect to loss
     # 机器学习里叫后向传播，通过梯度下降方法逐步逼近样本集特征
     # 需要高等数学的求导数
+	# 下面的方法，是最小二乘法，一种减小拟合函数和样本集误差的方法
     grad_y_pred = 2.0 * (y_pred - y)
     grad_a = grad_y_pred.sum()
     grad_b = (grad_y_pred * x).sum()
