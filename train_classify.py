4aeead6f94a798df37090da50e091f97c7db45b0
richard <whoami@hotmail.com>
2025-03-10 08:55:38 +0800

    debug

--- train_classify.py
+++ train_classify.py
@@ -11,10 +11,12 @@ from torch import nn
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
 class CustomDataset(Dataset):
     """
         自定义dataset，加载本地图片数据作为训练的样本数据
     """
+
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
@@ -32,7 +34,7 @@ class CustomDataset(Dataset):
                     # add image file 
                     self.image_list.append(os.path.join(class_dir, img_name))
                     # add label to image, must be number, not string, remember it, string can't be the element in a matrix
-                    #self.label_list.append(label)
+                    # self.label_list.append(label)
                     self.label_list.append(self.class_to_idx[label])  # 存储数值型标签
 
     def __len__(self):
@@ -42,17 +44,18 @@ class CustomDataset(Dataset):
         img_path = self.image_list[idx]
         image = Image.open(img_path).convert('RGB')
         label = self.label_list[idx]
-        
+
         if self.transform:
             image = self.transform(image)
 
         return image, label
-        
+
 
 class NeuralNetwork(nn.Module):
     """
     # Define a neural network as subclass of nn.Module used to train
     """
+
     def __init__(self):
         super().__init__()
         self.feature_extractor = nn.Sequential(
@@ -65,7 +68,7 @@ class NeuralNetwork(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(128*32*32, 256),
+            nn.Linear(128 * 32 * 32, 256),
             nn.Dropout(0.5),
             nn.Linear(256, 10)
         )
@@ -73,6 +76,7 @@ class NeuralNetwork(nn.Module):
     def forward(self, x):
         x = self.feature_extractor(x)
         return self.classifier(x)
+
 
 def train(dataloader, model, loss_fn, optimizer):
     """
@@ -94,35 +98,37 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")        
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-if __name__ =="__main__":
-    img_dir="./my_dataset/train"
-    print("start load my dataset in {}".format(img_dir))
+if __name__ == "__main__":
+
+def train_my_dt():
+    train_img_dir = "./my_dataset/train"
+    print("start load my dataset in {}".format(train_img_dir))
     # 对图像进行一些预处理和数据增强， 便于后期使用
     my_transform = transforms.Compose([
         transforms.RandomResizedCrop(128),  # 随机裁剪缩放
         transforms.ColorJitter(0.4, 0.4, 0.4),  # 颜色抖动
         transforms.RandomHorizontalFlip(),  # 随机水平翻转图像
-        transforms.Resize((128, 128)),      # Resize to 128x128
-        transforms.ToTensor(),              # Convert to tensor
+        transforms.Resize((128, 128)),  # Resize to 128x128
+        transforms.ToTensor(),  # Convert to tensor
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize, 对图像进行归一化处理，即减去均值并除以标准差
     ])
     # 创建 CustomDataset 实例，加载训练数据
-    train_dataset = CustomDataset(root_dir=img_dir, transform=my_transform)
+    train_dataset = CustomDataset(root_dir=train_img_dir, transform=my_transform)
     # 创建 DataLoader 实例
     print("build my data loader")
     train_data_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
     # to check data loaded to train_loader
     for images, labels in train_data_loader:
         print(images.shape, labels)
-    
+
     for X, y in train_data_loader:
         print(f"Shape of X [N, C, H, W]: {X.shape}")
         print(f"y: {y}")
         break
-        
+
     # Get cpu or gpu device for training.
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
@@ -132,9 +138,9 @@ if __name__ =="__main__":
     print("get a loss function")
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    epochs = 5
+    epochs = 50
     for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
+        print(f"Epoch {t + 1}\n-------------------------------")
         train(train_data_loader, model, loss_fn, optimizer)
     torch.save(model.state_dict(), "model.pth")
     print("saved PyTorch Model State to model.pth")
